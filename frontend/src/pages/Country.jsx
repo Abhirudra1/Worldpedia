16a8d3659ca5972b2fba840da4e22bb6ee7c1b58
Abhinav Singh <abhisingh622000@gmail.com>
2024-12-30 00:09:11 +0530

    All Country page done

--- frontend/src/pages/Country.jsx
+++ frontend/src/pages/Country.jsx
@@ -1,8 +1,28 @@
+import { useEffect, useState, useTransition } from "react"
+import { getCountryData } from "../api/postApi";
+import Loader from "../components/UI/Loader";
+import CountryCard from "../components/Layout/CountryCard";
 
 export default function Country() {
+  const [isPending, startTransition] = useTransition()
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    startTransition(async () => {
+      const res = await getCountryData()
+      setCountries(res.data)
+    });
+  }, []);
+
+  if(isPending) return <Loader />
+
   return (
-    <div>
-        <h1>Country Page</h1>
-    </div>
+    <section className="country-section container ">
+      <ul className="grid grid-four-cols">
+        {countries.map((curcountry, index) => {
+          return <CountryCard country={curcountry} key={index} /> 
+        })}
+      </ul>
+    </section>
   )
 }
